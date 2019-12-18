a91fb8c8c30c329a1bf5039d5c835853649ff685
jinouchi <my.unique.screen.name@gmail.com>
2019-12-18 13:20:33 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,29 +2,47 @@
 # Author: https://github.com/jinouchi
 # This script installs Wazuh Server on Debian systems.
 
+if [[ $(whoami) != "root" ]]
+then
+  echo "This script must be run as root. Exiting."
+  exit
+  else echo "You are root! Continuing..."
+fi
+  
 # Variables:
 DEB_VER=$(cat /etc/issue | sed 's/Debian GNU\/Linux \(.*\) \\n \\l/\1/')
 if [ $DEB_VER -gt 6 ] 
 then
   echo "Debian version detected successfully. Detected version: $DEB_VER"
 else
-  echo "Debian version could not be detected. Exiting."
+  echo "Debian version could not be detected from /etc/issue. Exiting."
   exit
 fi
 
-# Add the Wazuh repo
-# Install pre-requisites
-apt-get update
-apt-get install curl apt-transport-https lsb-release gnupg2 sudo -y
-
-# Install the GPG key:
+# Add Repos:
+# Install Wazuh GPG key:
 curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | apt-key add -
 
-# Add the repository:
+# Add Wazuh repository:
 echo "deb https://packages.wazuh.com/3.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
 
-# Update the package information:
+# Add the Elastic repository and its GPG key:
+apt-get install curl apt-transport-https
+curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
+
+# Add the Elastic repository and its GPG key:
+apt-get install curl apt-transport-https
+curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
+
+# Refresh apt 
 apt-get update
+
+
+# Wazuh
+# Install pre-requisites
+apt-get install curl apt-transport-https lsb-release gnupg2 -y
 
 # Install Wazuh server
 apt-get install wazuh-manager -y
@@ -40,22 +58,13 @@ fi
 
 apt-get install nodejs
 
+
 # Install the Wazuh API. It will update NodeJS if it is required:
 apt-get install wazuh-api
 
-# Disable Wazuh repository in order to prevent accidental upgrades (recommended):
-sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
-apt-get update
 
-
-# Install Filebeat
-# Add the Elastic repository and its GPG key:
-apt-get install curl apt-transport-https
-curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
-apt-get update
-
-#Install Filebeat:
+# Filebeat
+# Install Filebeat:
 apt-get install filebeat=7.5.0
 
 # Download the Filebeat config file from the Wazuh repository. This is pre-configured to forward Wazuh alerts to Elasticsearch:
@@ -65,7 +74,7 @@ curl -so /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/wazuh/wazu
 curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/v3.10.2/extensions/elasticsearch/7.x/wazuh-template.json
 
 # Download the Wazuh module for Filebeat:
-curl -s https://packages.wazuh.com/3.x/filebeat/wazuh-filebeat-0.1.tar.gz | sudo tar -xvz -C /usr/share/filebeat/module
+curl -s https://packages.wazuh.com/3.x/filebeat/wazuh-filebeat-0.1.tar.gz | tar -xvz -C /usr/share/filebeat/module
 
 # Enable and start the Filebeat service:
 # For Systemd:
@@ -73,18 +82,8 @@ systemctl daemon-reload
 systemctl enable filebeat.service
 systemctl start filebeat.service
 
-# For SysV Init:
-update-rc.d filebeat defaults 95 10
-service filebeat start
-
 
 # Install Elastic Stack
-# Add the Elastic repository and its GPG key:
-apt-get install curl apt-transport-https
-curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
-apt-get update
-
 # Install the Elasticsearch package:
 apt-get install elasticsearch=7.5.0
 
@@ -102,15 +101,21 @@ filebeat setup --index-management -E setup.template.json.enabled=false
 apt-get install kibana=7.5.0
 
 # Install the Wazuh app plugin for Kibana:
-sudo -u kibana /usr/share/kibana/bin/kibana-plugin install file:///path/wazuhapp-3.10.2_7.5.0.zip
+su - kibana -c '/usr/share/kibana/bin/kibana-plugin install file:///path/wazuhapp-3.10.2_7.5.0.zip'
 
 # Enable and start the Kibana service:
 systemctl daemon-reload
 systemctl enable kibana.service
 systemctl start kibana.service
 
+# Cleanup
+# Disable Wazuh repository in order to prevent accidental upgrades (recommended):
+sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/wazuh.list
+
 # Disable the Elasticsearch updates:
 sed -i "s/^deb/#deb/" /etc/apt/sources.list.d/elastic-7.x.list
+
+# Refresh apt
 apt-get update
 
 
